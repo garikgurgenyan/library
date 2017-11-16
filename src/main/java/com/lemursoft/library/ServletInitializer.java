264d29aa5672c9ff3d63c23ea75e9ba4f4518335
Gurgenyan Garik <g.gurgenyan@gmail.com>
2017-11-16 17:05:28 +0400

    Has been added ServletInitializer class

---
+++ src/main/java/com/lemursoft/library/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lemursoft.library;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = {"com.lemursoft.library"})
+public class ServletInitializer extends SpringBootServletInitializer {
+
+}
