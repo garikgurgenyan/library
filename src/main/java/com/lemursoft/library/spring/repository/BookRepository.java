030d906cb7db28a4bc4ce493a10b148f76736360
Gurgenyan Garik <g.gurgenyan@gmail.com>
2017-11-17 17:28:39 +0400

    Has been add repository layer

---
+++ src/main/java/com/lemursoft/library/spring/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.lemursoft.library.spring.repository;
+
+import com.lemursoft.library.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByNameContainingIgnoreCaseOrAuthorFioContainingIgnoreCaseOrderByName(String name, String fio);
+
+}
---
+++ src/main/java/com/lemursoft/library/spring/repository/PublisherRepository.java
@@ -0,0 +1,13 @@
+package com.lemursoft.library.spring.repository;
+
+import com.lemursoft.library.domain.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long>{
+
+    List<Publisher> findByNameContainingIgnoreCaseOrderByName(String name);
+}
